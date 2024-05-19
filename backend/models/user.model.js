fff05417273324e1d0f05049fb3869c6d8212abc
Girish Girijan <girishvazheliparambil@gmail.com>
2024-05-19 16:10:16 -0300

    Created User and Trasnsaction models. Implemented database connection

---
+++ backend/models/user.model.js
@@ -0,0 +1,31 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    profilePicture: {
+      type: String,
+      default:
+        "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_640.png",
+    },
+    gender: {
+      type: String,
+      enum: ["male", "female"],
+    },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("User", userSchema);
